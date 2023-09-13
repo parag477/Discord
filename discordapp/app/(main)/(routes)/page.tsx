f562aa0a12d6d49f2e3f71304611c02998b886a7
Parag <paragagrawal477@gmail.com>
2023-09-13 16:16:12 +0530

    Setup Dark & Light Themes ☀

--- discordapp/app/(main)/(routes)/page.tsx
+++ discordapp/app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>
       lorem ipsum
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
