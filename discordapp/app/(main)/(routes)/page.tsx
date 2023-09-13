bd45b83206525900b1221787fbff45a58717fbf0
Parag <paragagrawal477@gmail.com>
2023-09-13 15:59:01 +0530

    Authentication bug fixed 🧹

--- discordapp/app/(main)/(routes)/page.tsx
+++ discordapp/app/(main)/(routes)/page.tsx
@@ -1,10 +1,10 @@
-import { UserButton } from "@clerk/nextjs"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
+      lorem ipsum
       <UserButton afterSignOutUrl="/" />
-      
     </div>
   )
 }
