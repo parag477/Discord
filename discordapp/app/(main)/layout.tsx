41d6d609db8fa1080faa889b71baa11d6d240c97
Parag <paragagrawal477@gmail.com>
2023-09-23 20:13:45 +0530

    sidebar created 🎀

--- discordapp/app/(main)/layout.tsx
+++ discordapp/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+
 const MainLayout = async ({
     children
 }: {
@@ -5,7 +7,12 @@ const MainLayout = async ({
 }) => {
   return (
     <div className="h-full">
-      {children}
+        <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+            <NavigationSidebar />
+        </div>
+        <main className="md:pl-[72px] h-full">
+            {children}
+        </main>
     </div>
   );
 }
