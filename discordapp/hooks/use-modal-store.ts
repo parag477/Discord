ee00f7a412a9a76130035a33b0c5661f36750b22
Parag <paragagrawal477@gmail.com>
2024-01-02 21:01:28 +0530

    done server settings ⚙

--- discordapp/hooks/use-modal-store.ts
+++ discordapp/hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand"
 
-export type ModalType = "createserver" | "invite";
+export type ModalType = "createserver" | "invite" | "editServer";
 
 interface ModalData {
     server?: Server
