e71f025d8465d378953ed42bad7aa75161874a0f
parag477 <paragagrawal477@gmail.com>
2024-01-26 23:47:55 +0530

    delete chat ❌

--- discordapp/hooks/use-modal-store.ts
+++ discordapp/hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand"
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage";
 
 interface ModalData {
     server?: Server;
@@ -18,7 +18,7 @@ interface ModalData {
     onOpen: (type: ModalType, data?: ModalData) => void;
     onClose: () => void;
   }
-  
+
   export const useModal = create<ModalStore>((set) => ({
     type: null,
     data: {},
